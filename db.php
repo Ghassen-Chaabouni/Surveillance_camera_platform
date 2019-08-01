f7a1e5e7a4fc71abae96fc212a3dc4968e78ed0a
Ghassen Chaabouni <ghassen1302@live.com>
2019-08-01 22:08:23 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,9 @@
 <?php
-
-	 function deleteRecord(mysqli $db, $time){
-		$sql ="DELETE FROM `database` WHERE Time='".$time."'";
-		$result = $db->query($sql);
-		if(!$result){
-			throw new Exception('Cannot delete');
-		}
+    function deleteRecord(mysqli $db, $time){
+	$sql ="DELETE FROM `database` WHERE Time='".$time."'";
+	$result = $db->query($sql);
+	if(!$result){
+		throw new Exception('Cannot delete');
 	}
-
+     }
 ?>
