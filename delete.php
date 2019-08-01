768a5a41231e052c9d4d19f0b05df71f1ac13199
Ghassen Chaabouni <ghassen1302@live.com>
2019-08-01 22:12:26 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,38 +1,36 @@
  <?php
-	require_once 'db.php';
+     require_once 'db.php';
 	
-	$servername = "localhost:3306";
-	$username = "root";
-	$password = "root";
-	$databaseName = "database";
+     $servername = "localhost:3306";
+     $username = "root";
+     $password = "root";
+     $databaseName = "database";
 
-	$conn = new mysqli($servername,$username,$password,$databaseName);
+     $conn = new mysqli($servername,$username,$password,$databaseName);
 	 
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	} 
+     if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+     } 
 	
-	$time='';
-	if(!empty($_GET['Time'])){
-		$time = $_GET['Time'];
-	}
-	if(empty(time)){
-		throw new Exception('ID is blank');
-	}
-	deleteRecord($conn,$time);
+     $time='';
+     if(!empty($_GET['Time'])){
+	$time = $_GET['Time'];
+     }
+     if(empty(time)){
+	throw new Exception('ID is blank');
+     }
+     deleteRecord($conn,$time);
 
-	$picture='';
-	if(!empty($_GET['Picture'])){
-		$picture = $_GET['Picture'];
-	}
-	if(empty($picture)){
-		throw new Exception('Picture is blank');
-	}
+     $picture='';
+     if(!empty($_GET['Picture'])){
+	$picture = $_GET['Picture'];
+     }
+     if(empty($picture)){
+	throw new Exception('Picture is blank');
+     }
 
-    unlink($picture);
+     unlink($picture);
 
-
-	header("Location: /index.php");
-	die;
-  
+     header("Location: /index.php");
+     die;
 ?>
