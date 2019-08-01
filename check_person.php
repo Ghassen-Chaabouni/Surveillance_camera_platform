5edbfc621df0e926f64262b03401a08300154ce4
Ghassen Chaabouni <ghassen1302@live.com>
2019-08-01 22:06:57 +0100

    Update check_person.php

--- check_person.php
+++ check_person.php
@@ -1,5 +1,4 @@
 <?php
-
     $pyscript = 'C:\\MAMP\\htdocs\\check_person.py';
     $python = 'C:\\Users\\admin\\AppData\\Local\\Programs\\Python\\Python37\\python.exe';
     $cmd = "$python $pyscript";
