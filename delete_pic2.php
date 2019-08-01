6b949ef2d8423f4d109bbfe85d12ff820f2d0b9a
Ghassen Chaabouni <ghassen1302@live.com>
2019-08-01 22:17:08 +0100

    Update delete_pic2.php

--- delete_pic2.php
+++ delete_pic2.php
@@ -1,21 +1,19 @@
 <?php
-
-$pic='';
-if(!empty($_GET['pic'])){
+    $pic='';
+    if(!empty($_GET['pic'])){
 	$pic = $_GET['pic'];
-}
-if(empty(pic)){
+    }
+    if(empty(pic)){
 	throw new Exception('Error');
-}
+    }
 
-unlink($pic);
-$dirname = dirname($pic);
-$files = scandir($dirname);
-if (count($files)<=2){
-    array_map('unlink', glob("$dirname/*.*"));
-    rmdir($dirname);
-}
+    unlink($pic);
+    $dirname = dirname($pic);
+    $files = scandir($dirname);
+    if (count($files)<=2){
+        array_map('unlink', glob("$dirname/*.*"));
+        rmdir($dirname);
+    }
 
-
-header("Location: /junk.php");
+    header("Location: /junk.php");
 ?>
