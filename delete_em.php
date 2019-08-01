0eae863a3af762110e5578ca4dccff3792f63e21
Ghassen Chaabouni <ghassen1302@live.com>
2019-08-01 22:14:38 +0100

    Update delete_em.php

--- delete_em.php
+++ delete_em.php
@@ -1,19 +1,18 @@
 <?php
-	require_once 'db_em.php';
+    require_once 'db_em.php';
 
-	$servername = "localhost:3306";
-	$username = "root";
-	$password = "root";
-	$databaseName = "image";
+    $servername = "localhost:3306";
+    $username = "root";
+    $password = "root";
+    $databaseName = "image";
 
-	$conn = new mysqli($servername,$username,$password,$databaseName);
+    $conn = new mysqli($servername,$username,$password,$databaseName);
 
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
+    if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+    }
 
-        function RemoveAll ( $path ) {
-
+    function RemoveAll ( $path ) {
         foreach ( new DirectoryIterator($path) as $item ):
             if ( $item->isFile() ) unlink($item->getRealPath());
             if ( !$item->isDot() && $item->isDir() ) RemoveAll($item->getRealPath());
@@ -25,18 +24,16 @@
         RemoveAll('worker faces'.'/'.$_GET['name']);
     }
 
-	$id='';
-	if(!empty($_GET['id'])){
-		$id = $_GET['id'];
-	}
-	if(empty(id)){
-		throw new Exception('ID is blank');
-	}
+    $id='';
+    if(!empty($_GET['id'])){
+	$id = $_GET['id'];
+    }
+    if(empty(id)){
+	throw new Exception('ID is blank');
+    }
 
-	$querry="DELETE FROM images WHERE id='".$id."'";
+    $querry="DELETE FROM images WHERE id='".$id."'";
     mysqli_query($conn,$querry);
-
-
 
     header("Location: /add_an_employee.php");
     die;
