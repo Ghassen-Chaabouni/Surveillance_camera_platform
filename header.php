950ebbf256409bd80596f077c9b8eaabefff6c3a
salma <salmaa.frikha@etudiant-enit.utm.tn>
2019-08-04 15:28:45 +0100

    added a registration.php this is for the sign up

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" >
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<header>
+	<nav>
+		<div class="main-wrapper">
+			<ul>
+				<li><a href="index3.php">Home</a></li>
+			</ul>	
+			<div class="nav-login">
+				<form>
+					<input type="text" name="uid" placeholder="Username/e-mail">
+					<input type="password" name="pwd" placeholder="password">
+					<button type="submit" name="submit">login</button>
+				</form>
+				<a href="registration.php">Sign up</a>
+			</div>
+	</nav>
+</header>
