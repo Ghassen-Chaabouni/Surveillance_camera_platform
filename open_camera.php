860af5691c1617091d2d7270c78ea54123bdd5c8
Ghassen Chaabouni <ghassen1302@live.com>
2019-08-08 10:30:00 +0100

    Delete open_camera.php

--- open_camera.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $pyscript = 'C:\\MAMP\\htdocs\\open_camera.py';
-    $python = 'C:\\Users\\admin\\AppData\\Local\\Programs\\Python\\Python37\\python.exe';
-    $cmd = "$python $pyscript";
-    exec("$cmd", $output1);
-
-    header("Location: /index.php");
-?>
